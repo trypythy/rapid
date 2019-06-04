21eba0db1573516214677a27bc9d6e7800fcd852
Brijesh Tripathi <brijesht@gmail.com.com>
2019-06-04 20:22:03 +0530

    chap 2: templates

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,18 @@
 from application import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return ('Try try try again!')
+	user = {'username': 'Buster'}
+	reviews = [
+        {
+            'author': {'username': 'Jaya'},
+            'body': 'Beautiful day in Philadelphia!'
+        },
+        {
+            'author': {'username': 'Buster'},
+            'body': 'this is going to be a ride for sure'
+        }
+    ]
+	return render_template('index.html', title='Home', user=user, reviews=reviews)
